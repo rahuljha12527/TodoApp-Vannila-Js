bfddeed4904ccd99f847a90a00a403ff764794a9
rahuljha661 <rahuljha12527@gmail.com>
2020-08-18 00:36:53 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+// Selectors
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+
+// EVent Listener
+todoButton.addEventListener("click", addTodo);
+
+// Functions
+
+function addTodo(event) {
+  event.preventDefault();
+
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
+  //    Create li
+  const newTodo = document.createElement("li");
+  newTodo.innerText = todoInput.value;
+  newTodo.classList.add("todo-item");
+  todoDiv.appendChild(newTodo);
+//   Check Mark Button
+   const completedButton=document.createElement('button');
+   completedButton.innerHTML='<i class="fas fa-check"></i>';
+   completedButton.classList.add("complete-btn");
+   todoDiv.appendChild(completedButton);
+
+//  Trash Button
+    const trashButton=document.createElement('button');
+    trashButton.innerHTML='<i class="fas fa-trash"></i>';
+    trashButton.classList.add("trash-btn");
+    todoDiv.appendChild(trashButton);
+
+    // APPEND TO LIST
+    todoList.appendChild(todoDiv);
+
+
+    todoInput.value="";
+
+
+
+}
